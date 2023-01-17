e2c7fe4a5de45545a9a18d4a8cc69bc977501442
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-01-17 19:45:55 +0000

    added authentication

--- client/index.js
+++ client/index.js
@@ -53,15 +53,64 @@ async function initialiseAuth0Client() {
     });
 }
 
+async function updateAuthenticatedUI() {
+    const isAuthenticated = await auth0.isAuthenticated();
+    document.getElementById('login').disabled = isAuthenticated;
+    document.getElementById('logout').disabled = !isAuthenticated;
+    console.log({ isAuthenticated });
+    if (isAuthenticated) {
+      const user = await auth0.getUser();
+      const getTag = document.querySelector('#greeting');
+      getTag.textContent = `Welcome ${user.name}!`;
+    }
+}
+
+async function login(){
+    await auth0.loginWithRedirect({
+        redirect_uri: window.location.origin,
+    });
+}
+
+function logout(){
+    auth0.logout({
+        returnTo: window.location.origin,
+    });
+}
+
+async function handleRedirectCallback() {
+    const isAuthenticated = await auth0.isAuthenticated();
+    if(isAuthenticated) return;
+    const query = window.location.search;
+    if(query.includes('state=')){
+        try{
+            await auth0.handleRedirectCallback();
+        } catch(e){
+            window.alert(e.message || 'Not authenticated :/ ');
+            logout();
+        }
+        window.history.replaceState({}, document.title, '/');
+        await updateAuthenticatedUI();
+    }
+}
+
+function handleCalls(){
+    document.getElementById('login').addEventListener('click', login);
+    document.getElementById('logout').addEventListener('click', logout);
+}
+
 async function initialise() {
-    await initialiseAuth0Client();
     console.log('auth0 initialized');
     console.log({ auth0 });
+    await initialiseAuth0Client();
+    await handleCalls();
+    await handleRedirectCallback();
+    await updateAuthenticatedUI();
 }
-  
+
 window.addEventListener('load', initialise);
 
 // reference
 // Auth0: Secure access for everyone. But not just anyone.. Auth0. (2013). Retrieved 15 January 2023, from https://auth0.com/.
 // GitHub - portsoc/auth0-example. GitHub. (2021). Retrieved 15 January 2023, from https://github.com/portsoc/auth0-example.
 
+// Dhillon, G. (2022, April 29). Gurvirdhillon/Legoworkstation-CW. GitHub. Retrieved from https://github.com/gurvirdhillon/LegoWorkStation-CW 
