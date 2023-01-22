25733833c3048fa5ff2488a229973ae030a96c29
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-01-22 15:39:10 +0000

    corrected mistake from database

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -16,8 +16,9 @@ CREATE TABLE medication(
     medication_frequency varchar(60) NOT NULL,
     reminder_set BOOLEAN NOT NULL,
     reminder_notice varchar(60) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
     PRIMARY KEY(medication_id),
-    CONSTRAINT "fk_user_profile.user_email"
+    CONSTRAINT "fk_medication.user_email"
         FOREIGN KEY ("user_email")
         REFERENCES "user_profile"("user_email")
 );
@@ -26,9 +27,10 @@ CREATE TABLE nextOfKin(
     nextOfKin_email VARCHAR(255) PRIMARY KEY,
     first_name varchar(60) NOT NULL,
     last_name varchar(60) NOT NULL,
-    telephone_no varchar(20) NOT NULL
+    telephone_no varchar(20) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
     PRIMARY KEY(nextOfKin_email),
-    CONSTRAINT "fk_user_profile.user_email"
+    CONSTRAINT "fk_nextOfKin.user_email"
         FOREIGN KEY ("user_email")
         REFERENCES "user_profile"("user_email")
 );
@@ -37,10 +39,10 @@ CREATE TYPE assistance_need AS ENUM('fall', 'fire', 'medical', 'other');
 
 CREATE TABLE panic_button(
     panic_id SERIAL PRIMARY KEY,
-    nextOfKin_Email FOREIGN KEY REFERENCES nextOfKin(nextOfKin_email),
-    assistance_need assistance_need NOT NULL
+    "assistance_need" assistance_need NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
     PRIMARY KEY(panic_id),
-    CONSTRAINT "fk_user_profile.user_email"
+    CONSTRAINT "fk_panic_button.user_email"
         FOREIGN KEY ("user_email")
         REFERENCES "user_profile"("user_email")
 );
@@ -52,8 +54,9 @@ CREATE TABLE reminder_set(
     reminder_date varchar(60) NOT NULL,
     reminder_set BOOLEAN NOT NULL,
     reminder_notice varchar(60) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
     PRIMARY KEY(reminder_id),
-    CONSTRAINT "fk_user_profile.user_email"
+    CONSTRAINT "fk_reminder_set.user_email"
         FOREIGN KEY ("user_email")
         REFERENCES "user_profile"("user_email")
 );
