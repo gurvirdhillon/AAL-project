6a23c280f67bbad2a72b2a6ce408519134857a74
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-01-25 17:28:56 +0000

    need to debug strange error with commented code

--- client/index.js
+++ client/index.js
@@ -98,18 +98,18 @@ function handleCalls() {
   document.getElementById('logout').addEventListener('click', logout);
 }
 
-async function generateServiceWorker() {
-  try {
-    await navigator.serviceWorker.register('/worker.js');
-  } catch (e) {
-    console.error('Sorry, this service is not available at the moment.', e);
-  }
-}
+// async function generateServiceWorker() {
+//   try {
+//     await navigator.serviceWorker.register('/worker.js');
+//   } catch (e) {
+//     console.error('Sorry, this service is not available at the moment.', e);
+//   }
+// }
 
 
 async function initialise() {
-  await generateServiceWorker();
-  setInterval(1000);
+//   await generateServiceWorker();
+//   setInterval(1000);
   await initialiseAuth0Client();
   await handleCalls();
   await handleRedirectCallback();
