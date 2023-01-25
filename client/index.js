96fb718e95a3bf44cb0a872249ed19e6d0829e6d
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-01-25 17:56:05 +0000

    added offline file to seperate auth0 content

--- client/index.js
+++ client/index.js
@@ -98,18 +98,7 @@ function handleCalls() {
   document.getElementById('logout').addEventListener('click', logout);
 }
 
-// async function generateServiceWorker() {
-//   try {
-//     await navigator.serviceWorker.register('/worker.js');
-//   } catch (e) {
-//     console.error('Sorry, this service is not available at the moment.', e);
-//   }
-// }
-
-
 async function initialise() {
-//   await generateServiceWorker();
-//   setInterval(1000);
   await initialiseAuth0Client();
   await handleCalls();
   await handleRedirectCallback();
---
+++ client/offline.js
@@ -0,0 +1,15 @@
+async function generateServiceWorker() {
+    try {
+      await navigator.serviceWorker.register('/worker.js');
+    } catch (e) {
+      console.error('Sorry, this service is not available at the moment.', e);
+    }
+  }
+
+
+async function init() {
+    await generateServiceWorker();
+    setInterval(1000);
+}
+
+window.addEventListener('load', init);
