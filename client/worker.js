8b5cf569df7d106dea3881e04c7cba560b9108f1
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-01-26 14:29:58 +0000

    issue with worker file

--- client/worker.js
+++ client/worker.js
@@ -1,15 +1,16 @@
-// const filesHandler = [
-//   '/index.html',
-//   '/index.js',
-//   '/index.css',
-//   '/worker.js',
-//   '/offline.js',
-//   '/manifest.json',
-//   '/img/elderly-technology.png',
-// ];
+const CACHE = 'AAL';
+const filesHandler = [
+  '/index.html',
+  '/index.js',
+  '/index.css',
+  '/worker.js',
+  '/offline.js',
+  '/manifest.json',
+  '/img/elderly-technology.png',
+];
 
 async function fetchFromCache(request) {
-  const cache = await caches.open('AAL');
+  const cache = await caches.open('cacheName');
   const data = await cache.match(request);
   if (data) {
     return data;
@@ -24,11 +25,17 @@ function interceptFetch(e) {
   e.respondWith(fetchFromCache(e.request));
 }
 
-function getServiceWorker() {
-  console.log('Service worker registered');
+async function getServiceWorker() {
+  try {
+    const c = await caches.open(CACHE);
+    await c.addAll(filesHandler);
+    console.log('Service worker registered');
+  } catch (e) {
+    console.log('cache failed, sorry ', e);
+  }
 }
 
-self.addEventListener('fetch', interceptFetch);
 self.addEventListener('install', getServiceWorker);
+self.addEventListener('fetch', interceptFetch);
 
 // Kopecky, J., &amp; Boakes, R. (2021, May 27). Portsoc/deadline: A deadline countdown. GitHub. Retrieved from https://github.com/portsoc/deadline
