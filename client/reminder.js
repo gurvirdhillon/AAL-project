f2b308821c8b135017abd6a0be0ccce1cef32db3
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-02-09 23:58:00 +0000

    removed file css - starting over

--- client/reminder.js
+++ client/reminder.js
@@ -1,17 +1,12 @@
 const createList = document.querySelector('#createList');
 const reminderDate = document.querySelector('#reminderDate');
 const handleSubmitBtn = document.querySelector('#submitBtn');
+const article = document.getElementsByTagName('article')[0];
 
-handleSubmitBtn.addEventListener('click', dateValidator);
-
-// find the list of reminders for that day and add those reminders
-
-function dateValidator() {
-  const inputDate = new Date(document.querySelector('#reminderDate').value);
-  const today = new Date();
-  if (inputDate < today) {
-    return false;
-  } else {
-    return true;
-  }
+if(handleSubmitBtn){
+handleSubmitBtn.addEventListener('click', function () {
+  // show the sticky note element
+  const stickyNote = document.querySelector('#stickyNote');
+  article.innerHTML = stickyNote.innerHTML;
+});
 }
