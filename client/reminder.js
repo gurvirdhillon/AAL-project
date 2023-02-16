09bcb01d5ca136c9eb065834fdd7cbedbad00dd0
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-02-16 14:41:32 +0000

    added reminder feature to main page - button now working

--- client/reminder.js
+++ client/reminder.js
@@ -1,12 +1,51 @@
-const createList = document.querySelector('#createList');
-const reminderDate = document.querySelector('#reminderDate');
-const handleSubmitBtn = document.querySelector('#submitBtn');
-const article = document.getElementsByTagName('article')[0];
+// 
 
-if(handleSubmitBtn){
-handleSubmitBtn.addEventListener('click', function () {
-  // show the sticky note element
-  const stickyNote = document.querySelector('#stickyNote');
-  article.innerHTML = stickyNote.innerHTML;
-});
-}
+// const getInput = document.querySelector('#reminderInput');
+// const reminderDate = document.querySelector('#reminderDate');
+// const getSubmit = document.querySelector('#submitBtn');
+// const targetContent = document.querySelector('#createList');
+// const getForm = document.querySelector('#reminderFrm');
+
+// if (getForm) {
+//   getForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const context = getInput.value;
+//     const date = reminderDate.value;
+//     reminderInput.value = '';
+//     reminderDate.value = '';
+
+//     const giveList = document.createElement('li');
+//     giveList.className = 'list-group-item';
+//     giveList.textContent = `reminder to: ${context} at ${date}`;
+//     targetContent.appendChild(giveList);
+//     const getDate = document.createElement('span');
+//     getDate.classList.add('reminderDate');
+//     getDate.textContent = date;
+//     giveList.appendChild(getDate);
+//   });
+// }
+
+// const getInput = document.querySelector('#reminderInput');
+// const reminderDate = document.querySelector('#reminderDate');
+// const getSubmit = document.querySelector('#submitBtn');
+// const targetContent = document.querySelector('#createList');
+// const getForm = document.querySelector('#reminderFrm');
+
+// if(getForm) {
+//   getForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const context = getInput.value;
+//     const date = reminderDate.value;
+//     reminderInput.value = '';
+//     reminderDate.value = '';
+
+//     const giveList = document.createElement('li');
+//     giveList.className = 'list-group-item';
+//     giveList.textContent = `reminder to: ${context} at ${date}`;
+//     targetContent.appendChild(giveList);
+//     const getDate = document.createElement('span');
+//     getDate.classList.add('reminderDate');
+//     getDate.textContent = date;
+//     giveList.appendChild(getDate);
+//   });
+// }
