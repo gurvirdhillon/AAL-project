7a1b46bc910e00e13ebcd818da2f314476ce0ff9
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-02-23 13:32:56 +0000

    added message to the client(need further server side maintenance)

--- client/chat.js
+++ client/chat.js
@@ -1,57 +1,17 @@
-// start the chat by getting the input, pressing the send button then it displays it as a message on the page
+// const getMsgForm = document.querySelector('#msgForm');
+// const input = document.querySelector('#inputBar');
+// const sendMsg = document.querySelector('#sendMsg');
+// const displayMessages = document.querySelector('#showMessages');
 
-const grabDisplay = document.querySelector('#chatDisplay');
-const grabInputBar = document.querySelector('#inputMsg');
-const grabSendBtn = document.querySelector('#chatSendBtn');
-const grabFrm = document.querySelector('#MsgForm');
+// // prevent the default of the event
 
+// function getMessageOnScreen(e) {
+//   e.preventDefault();
+//   const msg = input.value;
+//   const makeMsg = document.createElement('p');
+//   makeMsg.textContent = msg;
+//   displayMessages.appendChild(makeMsg);
+//   input.value = '';
+// }
 
-(async () => {
-    const io = await import('socket.io');
-    const socket = io('http://localhost:8080');
-
-window.addEventListener('click', function () {
-  const getbtn = document.querySelector('#txtOpen');
-  const template = document.querySelector('#openChat');
-  const divHolder = document.querySelector('#PutChatHere');
-  if (getbtn) {
-    getbtn.addEventListener('click', function () {
-      console.log('button clicked');
-      // open the chat window
-      divHolder.innerHTML = template.innerHTML;
-    });
-  }
-});
-
-grabFrm.addEventListener('submit', function(e){
-    e.preventDefault();
-    const msg = grabInputBar.value;
-    socket.emit('send-message', msg);
-    grabInputBar.value = '';
-});
-
-socket.on('chat-message', data => {
-    console.log("Message received: " + data);
-  });
-});
-
-// function handleMsg() {
-
-function handleChat() {
-  const grabInputBar = document.querySelector('#inputMsg');
-  if(grabInputBar.value !== null) {
-  const message = grabInputBar.value;
-    getChatDisplay.textContent = message;
-  }
-};
-
-if (grabSendBtn) {
-  grabSendBtn.addEventListener('click', handleChat);
-}
-
-function initialising() {
-  handleChat();
-};
-
-window.addEventListener('load', initialising);
-
+// getMsgForm.addEventListener('submit', getMessageOnScreen);
