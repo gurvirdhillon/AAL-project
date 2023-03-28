ff4ece68ec4f428b79630d132551effd0f2e1e87
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-03-28 17:59:05 +0100

    panic button changes

--- client/panic.mjs
+++ client/panic.mjs
@@ -1,28 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const panicBtn = document.querySelector('#panicBtn');
-    panicBtn.addEventListener('click', async () => {
-      const check = confirm('Are you sure you want to send a message to your next of kin?');
-      if (check) {
+// document.addEventListener('DOMContentLoaded', () => {
+async function activatePanicButton() {
+  const panicBtn = document.querySelector('#panicBtn');
+  panicBtn.addEventListener('click', async () => {
+    const check = confirm('Are you sure you want to send a message to your next of kin?');
+    if (check) {
       const response = await fetch('/send-message', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           body: 'Help me!',
           from: '+447908632941',
-          to: '+447893943882'
-        })
+          to: '+447893943882',
+        }),
       });
       alert('Message sent to next of kin');
       const data = await response.json();
     } else {
       alert('Message not sent');
-    };
-    });
-    const data = await response.json();
-    console.log(data);
+    }
   });
+  const data = await response.json();
+  console.log(data);
+}
 
-
-
+window.addEventListener('load', activatePanicButton);
