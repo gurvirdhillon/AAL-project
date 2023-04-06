c7c31444055f5d6126bb31ad0914ab9d892e771d
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-04-06 14:49:37 +0100

    added medication delete button

--- client/medication.js
+++ client/medication.js
@@ -5,53 +5,52 @@ const strength = document.querySelector('#medicationStrength');
 
 const medicationList = document.querySelector('#medicationList');
 
-if(form){
-form.addEventListener('submit', function(event){
+if (form) {
+  form.addEventListener('submit', function (event) {
     event.preventDefault();
     const value = input.value;
-    if(!value){
-        return;
+    if (!value) {
+      return;
     }
-  localStorage.setItem('medicationInput', value);
-  localStorage.setItem('medicationDosage', dosage.value);
-  localStorage.setItem('medicationStrength', strength.value);
+    localStorage.setItem('medicationInput', value);
+    localStorage.setItem('medicationDosage', dosage.value);
+    localStorage.setItem('medicationStrength', strength.value);
     const medications = JSON.parse(localStorage.getItem('medications')) || [];
     medications.push({
-        medication: value,
-        dosage: dosage.value,
-        strength: strength.value,
-        time: document.querySelector('#medicationTime').value
+      medication: value,
+      dosage: dosage.value,
+      strength: strength.value,
+      time: document.querySelector('#medicationTime').value,
     });
     const medTime = document.querySelector('#medicationTime').value;
     const time = new Date();
     time.setHours(medTime.split(':')[0]);
     time.setMinutes(medTime.split(':')[1]);
-    if (Notification.permission !== "granted") {
-        Notification.requestPermission();
+    if (Notification.permission !== 'granted') {
+      Notification.requestPermission();
     }
-    setTimeout(function(){
-        new Notification(value +  " medication reminder");
-    }, 
+    setTimeout(function () {
+      new Notification(value + ' medication reminder');
+    },
     time - new Date());
     const makeList = document.createElement('li');
-    makeList.textContent = "medication reminder: " + value + ' to be consumed at ' + document.querySelector('#medicationTime').value;
+    makeList.textContent = 'medication reminder: ' + value + ' to be consumed at ' + document.querySelector('#medicationTime').value;
     medicationList.appendChild(makeList);
     // get the id of medicationlist to stay on the page after it refreshes
     // localStorage.setItem('medicationList', medicationList.id);
     localStorage.setItem('medications', JSON.stringify(medications));
     input.value = '';
-
   });
-};
+}
 
 window.addEventListener('load', function () {
   input.value = localStorage.getItem('medicationInput') || ' ';
   dosage.value = localStorage.getItem('medicationDosage') || ' ';
   strength.value = localStorage.getItem('medicationStrength') || ' ';
   // stores the users reminders in local storage
-  
+
   const medications = JSON.parse(localStorage.getItem('medications')) || [];
-  medications.forEach(function(reminder){
+  medications.forEach(function (reminder) {
     const item = document.createElement('li');
     item.textContent = 'medication reminder: ' + reminder.medication + ' to be consumed at ' + reminder.time;
     medicationList.appendChild(item);
@@ -88,12 +87,33 @@ const uploadedImage = document.getElementById('uploadedImage');
 
 fileInput.addEventListener('change', (event) => {
   const file = event.target.files[0];
-  
+
   const reader = new FileReader();
   reader.onload = () => {
     uploadedImage.src = reader.result;
     uploadedImage.style.display = 'block';
   };
-  
+
   reader.readAsDataURL(file);
 });
+
+// delete all the medications reminder
+
+function deleteAllMedication() {
+  const medicationList = document.querySelector('#medicationList');
+  // clear the reminders by using the medications key
+  if (medicationList !== null) {
+    const createDeleteBtn = document.createElement('button');
+    createDeleteBtn.textContent = 'delete all medication reminders';
+    medicationList.appendChild(createDeleteBtn);
+    createDeleteBtn.addEventListener('click', () => {
+      localStorage.removeItem('medications');
+      localStorage.removeItem('medicationInput');
+      localStorage.removeItem('medicationDosage');
+      localStorage.removeItem('medicationStrength');
+      medicationList.innerHTML = '';
+    });
+  }
+}
+
+window.addEventListener('load', deleteAllMedication);
