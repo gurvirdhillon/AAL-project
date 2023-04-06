90c36db00f4a1c724e7164b7683217379ecd14a3
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-04-06 15:33:24 +0100

    added first integrations for checking the local storage(no luck on implementation)

--- tests/function.test.js
+++ tests/function.test.js
@@ -1,16 +1,13 @@
 import { app } from '../server/server.js'
 
-// twilio api and checking if it is fully integrated and can send messages
-
-// let twilioTest = await fetch('http://localhost:8080/send-message');
-// await test('Testing Twilio endpoint', async () => {
-//   await expect(twilioTest.status).toEqual(200);
-// });
+import { JSDOM } from 'jsdom';
 
 let authConfig = await fetch('http://localhost:8080/auth-config');
 await test('Testing authorisation configuration endpoint', async () => {
   await expect(authConfig.status).toEqual(200);
 });
+
+// twilio api and checking if it is fully integrated and can send messages
 
 let twilioTest = await fetch('http://localhost:8080/send-message');
 await test('Testing Twilio endpoint', async () => {
@@ -18,12 +15,17 @@ await test('Testing Twilio endpoint', async () => {
   // as there is no url parameter, the status code should be 404
 });
 
-// test if the notification feature allows for inputting of notification and the time of the day to send the notification
+// test if it successfully added the reminder input, date and time to local storage
 
 
 
 
-// test if the medication feature allows for inputting of medication and the time of the day to take the medication
+// test if the medication has added the medication input and time to local storage
 
+test('see if medication had added input and time to local storage', () => {
+  expect(localStorage.getItem('medicationInput')).toBe('medicationInput');
+  expect(localStorage.getItem(`${medication.time}`)).toBe(`${medication.time}`);
+});
 
-
+// export default test;
+// export default app;
