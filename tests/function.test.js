ad128b8a3c8e831161182cd7cb8835bb68251991
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2023-05-16 00:06:49 +0100

    added code testing

--- tests/function.test.js
+++ tests/function.test.js
@@ -1,6 +1,6 @@
 import { app } from '../server/server.js'
-// import {  } from 'jest-localstorage-mock';
-// import { webextension }  from 'jest-webextension-mock';
+import {  } from 'jest-localstorage-mock';
+import { webextension }  from 'jest-webextension-mock';
 
 let authConfig = await fetch('http://localhost:8080/auth-config');
 await test('Testing authorisation configuration endpoint', async () => {
@@ -22,88 +22,88 @@ await test('Testing reminder.html if it renders', async () => {
 
 // test if it successfully added the reminder input, date and time to local storage
 
-// describe('Adding reminder to local storage', () => {
-//   test('local storage should hold the input, date and time', () => {
-//     // Set up test data
-//     const reminder = 'Test reminder';
-//     const date = '2023-04-12';
-//     const time = '10:30';
+describe('Adding reminder to local storage', () => {
+  test('local storage should hold the input, date and time', () => {
+    // Set up test data
+    const reminder = 'Test reminder';
+    const date = '2023-04-12';
+    const time = '10:30';
 
 
-//     // Add reminder to local storage
-//     localStorage.setItem('reminder', reminder);
-//     localStorage.setItem('date', date);
-//     localStorage.setItem('time', time);
+    // Add reminder to local storage
+    localStorage.setItem('reminder', reminder);
+    localStorage.setItem('date', date);
+    localStorage.setItem('time', time);
 
 
-//     // Retrieve reminder from local storage
-//     const storedReminder = localStorage.getItem('reminder');
-//     const storedDate = localStorage.getItem('date');
-//     const storedTime = localStorage.getItem('time');
+    // Retrieve reminder from local storage
+    const storedReminder = localStorage.getItem('reminder');
+    const storedDate = localStorage.getItem('date');
+    const storedTime = localStorage.getItem('time');
 
 
-//     // Assert that stored reminder matches input reminder
-//     expect(storedReminder).toEqual(reminder);
+    // Assert that stored reminder matches input reminder
+    expect(storedReminder).toEqual(reminder);
 
 
-//     // Assert that stored date matches input date
-//     expect(storedDate).toEqual(date);
+    // Assert that stored date matches input date
+    expect(storedDate).toEqual(date);
 
 
-//     // Assert that stored time matches input time
-//     expect(storedTime).toEqual(time);
-//   });
-// });
+    // Assert that stored time matches input time
+    expect(storedTime).toEqual(time);
+  });
+});
 
 // test if the medication has added the medication input and time to local storage
 
-// describe('adding medication to local storage should include the medication name and time', () => {
-//   test('local storage should hold the medication name and time', () => {
-//     // Set up test data
-//     const medication = 'Test medication';
-//     const time = '10:30';
+describe('adding medication to local storage should include the medication name and time', () => {
+  test('local storage should hold the medication name and time', () => {
+    // Set up test data
+    const medication = 'Test medication';
+    const time = '10:30';
 
-//     // Add medication to local storage
-//     localStorage.setItem('medication', medication);
-//     localStorage.setItem('time', time);
+    // Add medication to local storage
+    localStorage.setItem('medication', medication);
+    localStorage.setItem('time', time);
 
-//     // Retrieve medication from local storage
-//     const storedMedication = localStorage.getItem('medication');
-//     const storedTime = localStorage.getItem('time');
+    // Retrieve medication from local storage
+    const storedMedication = localStorage.getItem('medication');
+    const storedTime = localStorage.getItem('time');
 
-//     // Assert that stored medication matches input medication
-//     expect(storedMedication).toEqual(medication);
+    // Assert that stored medication matches input medication
+    expect(storedMedication).toEqual(medication);
 
-//     // Assert that stored time matches input time
-//     expect(storedTime).toEqual(time);
-//   });
-// });
+    // Assert that stored time matches input time
+    expect(storedTime).toEqual(time);
+  });
+});
 
 // testing if the notification feature works for the medication
 
-// describe('Notification behavior', () => {
-//   test('should display notification when triggered', async () => {
-//     // Set up test data
-//     const title = 'Test Notification';
-//     const message = 'This is a test notification';
-//     const options = {
-//       body: message,
-//       icon: 'test-icon.png'
-//     };
+describe('Notification behavior', () => {
+  test('should display notification when triggered', async () => {
+    // Set up test data
+    const title = 'Test Notification';
+    const message = 'This is a test notification';
+    const options = {
+      body: message,
+      icon: 'test-icon.png'
+    };
 
-//     // Trigger notification
-//     const notificationId = await webextension.notifications.create(title, options);
+    // Trigger notification
+    const notificationId = await webextension.notifications.create(title, options);
 
-//     // Assert that notification was created with correct title and message
-//     expect(webextension.notifications.get(notificationId)).resolves.toEqual(
-//       expect.objectContaining({
-//         type: 'basic',
-//         title: title,
-//         message: message
-//       })
-//     );
+    // Assert that notification was created with correct title and message
+    expect(webextension.notifications.get(notificationId)).resolves.toEqual(
+      expect.objectContaining({
+        type: 'basic',
+        title: title,
+        message: message
+      })
+    );
 
-//     // Close notification
-//     await webextension.notifications.clear(notificationId);
-//   });
-// });
+    // Close notification
+    await webextension.notifications.clear(notificationId);
+  });
+});
